a0e9dd4170543ac5a0bd2982b4a8d3925e03ce7c
nhutlee0504 <leminhnhut718@gmail.com>
2024-12-12 17:52:00 +0700

    Sau merge Nhut

---
+++ SanGiaoDich_BrotherHood/SanGiaoDich_BrotherHood/Server/Controllers/BillController.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SanGiaoDich_BrotherHood.Shared.Models;
+using SanGiaoDich_BrotherHood.Server.Services;
+using SanGiaoDich_BrotherHood.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SanGiaoDich_BrotherHood.Shared.Dto;
+using System;
+
+namespace SanGiaoDich_BrotherHood.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillController : ControllerBase
+    {
+        private IBill bill;
+        public BillController(IBill bill)
+        {
+            this.bill = bill;
+        }
+
+        [HttpGet("GetBill")]
+        public async Task<IActionResult> GetBills()
+        {
+            return Ok(await bill.GetBills());
+        }
+
+        [HttpGet("GetBillsByUserName/{userName}")]
+        public async Task<IActionResult> GetBillsByUserName(string userName)
+        {
+            return Ok(await bill.GetBillsByUserName(userName));
+        }
+
+        [HttpGet("GetBillsByIDBill/{IDBill}")]
+        public async Task<IActionResult> GetBillsByIDBill(int IDBill)
+        {
+            return Ok(await bill.GetBillByIDBill(IDBill));
+        }
+
+        [HttpPost("AddBill")]
+        public async Task<IActionResult> AddBill([FromBody] BillDto billDto)
+        {
+            if (billDto == null)
+            {
+                return BadRequest("Thông tin hóa đơn không hợp lệ.");
+            }
+
+            try
+            {
+                var newBill = await bill.AddBill(billDto);
+
+                // Trả về phản hồi thành công với thông tin hóa đơn mới được tạo
+                return Ok(newBill);
+            }
+            catch (Exception ex)
+            {
+                // Ghi log lỗi (nếu cần thiết) và trả về phản hồi lỗi
+                return StatusCode(500, $"Đã xảy ra lỗi khi tạo hóa đơn: {ex.Message}");
+            }
+        }
+
+
+        [HttpPut("IDBill")]
+        public async Task<ActionResult> UpdateBill(int IDBill, Bill bl)
+        {
+            return Ok(await bill.UpdateBill(IDBill, bl));
+        }
+
+        [HttpPost("AcceptBill/{idBill}")]
+        public async Task<IActionResult> AcceptBill(int idBill)
+        {
+            return Ok(await bill.AcceptBill(idBill));
+        }
+
+        [HttpPost("CancelBill/{idBill}")]
+        public async Task<IActionResult> CancelBill(int idBill)
+        {
+            return Ok(await bill.CancelBill(idBill));
+        }
+
+        [HttpPost("done/{id}")]
+        public async Task<IActionResult> DoneBill(int id, string status)
+        {
+            var bi = await bill.DoneBill(id, status);
+
+            if (bi == null)
+            {
+                return NotFound($"Bill with ID {id} not found.");
+            }
+
+            return Ok(bill);
+        }
+    }
+}
---
+++ SanGiaoDich_BrotherHood/SanGiaoDich_BrotherHood/Server/Services/IUser.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SanGiaoDich_BrotherHood.Shared.Dto;
+using SanGiaoDich_BrotherHood.Shared.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SanGiaoDich_BrotherHood.Server.Services
+{
+    public interface IUser
+    {
+        public Task<IEnumerable<Account>> GetAllAccount();
+        public Task<Account> RegisterUser(RegisterDto registerDto);
+        public Task<string> LoginUser(LoginDto loginDto);
+        public Task<Account> GetAccountInfo();
+        public Task<Account> GetAccountByUserName(string userName);
+		Task<Account> UpdateAccountInfo(string email);
+        Task<Account> UpdateAccountInfo2(string phone, string description);
+        Task<Account> UpdateProfileImage(IFormFile imageFile);
+        public Task<Account> ChangePassword(string username, InfoAccountDto info);
+        public Task<Account> AcceptIDCard(RecognitionDto recognitionDto);
+        Task<Dictionary<string, int>> GetUserStatisticsAsync();
+    }
+}
